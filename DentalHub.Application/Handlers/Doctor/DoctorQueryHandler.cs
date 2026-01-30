da69cbd3f9837363c9149f6cd526506d4d86ce48
Haytham859 <haythamhossamismail@gmail.com>
2026-01-30 23:48:41 +0200

    Update DoctorQueryHandler.cs

--- DentalHub.Application/Handlers/Doctor/DoctorQueryHandler.cs
+++ DentalHub.Application/Handlers/Doctor/DoctorQueryHandler.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace DentalHub.Application.Handlers.Doctor
 {
-    internal class DoctorQueryHandler
+  public class DoctorQueryHandler :
+    IRequestHandler<GetDoctorByIdQuery, Result<DoctorDto>>,
+    IRequestHandler<GetAllDoctorsQuery, Result<List<DoctorDto>>>
+{
+    private readonly IDoctorService _service;
+
+    public DoctorQueryHandler(IDoctorService service)
     {
+        _service = service;
     }
+
+    public Task<Result<DoctorDto>> Handle(GetDoctorByIdQuery request, CancellationToken ct)
+        => _service.GetByIdAsync(request.Id);
+
+    public Task<Result<List<DoctorDto>>> Handle(GetAllDoctorsQuery request, CancellationToken ct)
+        => _service.GetAllAsync();
 }
+
+}
+
