af1a679f2c18dd76e6d426c1d5ee8ba2914d2a86
Haytham859 <haythamhossamismail@gmail.com>
2026-01-30 19:12:28 +0200

    Update CreateDoctorCommand.cs

--- DentalHub.Application/Commands/Doctor/CreateDoctorCommand.cs
+++ DentalHub.Application/Commands/Doctor/CreateDoctorCommand.cs
@@ -1,12 +1,17 @@
-﻿using System;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MediatR;
 namespace DentalHub.Application.Commands.Doctor
 {
-    internal class CreateDoctorCommand
-    {
-    }
+    public record CreateDoctorCommand(
+    string Name,
+    Guid UserId,
+    string Specialty,
+    int UniversityId
+) : IRequest<Result<Guid>>;
+
 }
