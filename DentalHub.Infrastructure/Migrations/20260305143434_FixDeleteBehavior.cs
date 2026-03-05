12eb5e34d2ec1d876ce78b05876e7131a0626e42
omargamal1121 <omargamal1132004@gmail.com>
2026-03-05 22:27:32 +0200

    add-last-migration

---
+++ DentalHub.Infrastructure/Migrations/20260305143434_FixDeleteBehavior.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DentalHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDeleteBehavior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DropForeignKey(
+            //    name: "FK_Admins_AspNetUsers_Id",
+            //    table: "Admins");
+
+            //migrationBuilder.DropForeignKey(
+            //    name: "FK_Doctors_AspNetUsers_Id",
+            //    table: "Doctors");
+
+            //migrationBuilder.DropForeignKey(
+            //    name: "FK_Patients_AspNetUsers_Id",
+            //    table: "Patients");
+
+            //migrationBuilder.DropForeignKey(
+            //    name: "FK_Students_AspNetUsers_Id",
+            //    table: "Students");
+
+            //migrationBuilder.DropForeignKey(
+            //    name: "FK_UniversityMembers_Universities_UniversityId",
+            //    table: "UniversityMembers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Admins_AspNetUsers_Id",
+                table: "Admins",
+                column: "Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Doctors_AspNetUsers_Id",
+                table: "Doctors",
+                column: "Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Patients_AspNetUsers_Id",
+                table: "Patients",
+                column: "Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_AspNetUsers_Id",
+                table: "Students",
+                column: "Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UniversityMembers_Universities_UniversityId",
+                table: "UniversityMembers",
+                column: "UniversityId",
+                principalTable: "Universities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DropForeignKey(
+            //    name: "FK_Admins_AspNetUsers_Id",
+            //    table: "Admins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Doctors_AspNetUsers_Id",
+                table: "Doctors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Patients_AspNetUsers_Id",
+                table: "Patients");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_AspNetUsers_Id",
+                table: "Students");
+
+            //migrationBuilder.DropForeignKey(
+            //    name: "FK_UniversityMembers_Universities_UniversityId",
+            //    table: "UniversityMembers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Admins_AspNetUsers_Id",
+                table: "Admins",
+                column: "Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Doctors_AspNetUsers_Id",
+                table: "Doctors",
+                column: "Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Patients_AspNetUsers_Id",
+                table: "Patients",
+                column: "Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_AspNetUsers_Id",
+                table: "Students",
+                column: "Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UniversityMembers_Universities_UniversityId",
+                table: "UniversityMembers",
+                column: "UniversityId",
+                principalTable: "Universities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
